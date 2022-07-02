7d7f53dc95ac5fdc3b564716883201611c3a66f2
dory <ryoyadoi.kyoto@gmail.com>
2022-07-02 22:51:40 +0900

    edit server.js and add routes directory

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hello node.js");
+const express = require("express");
+const app = express();
+const  userRouter = require("./routes/user");
+
+const PORT = 3000;
+
+app.get("/", (req,res) => {
+    //console.log("helo node.js")
+    res.send("<h1>こんにちは</h1>");
+    //res.status(404).json({msg: "エラーです"});
+})
+
+//ルーティング
+app.use("/user", userRouter);
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));
