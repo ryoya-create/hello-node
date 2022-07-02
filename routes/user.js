1f6df81b1f104dd3a6b860ca3a56cf04d15196db
dory <ryoyadoi.kyoto@gmail.com>
2022-07-02 23:29:59 +0900

    add public directory

--- routes/user.js
+++ routes/user.js
@@ -11,4 +11,8 @@ router.get("/info", (req,res) =>{
     res.send("ユーザー情報です");
 })
 
+router.get("/:id", (req, res) =>{
+    res.send(`${req.params.id}のユーザー情報を取得しました`);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const  userRouter = require("./routes/user");
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req,res) => {
     //console.log("helo node.js")
-    res.send("<h1>こんにちは</h1>");
+    //res.send("<h1>こんにちは</h1>");
     //res.status(404).json({msg: "エラーです"});
 })
 
 //ルーティング
 app.use("/user", userRouter);
+//app.use("/auth", authRouter);
+//app.use("/customer", customerRouter);
+//app.use("/product", productRouter);
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
