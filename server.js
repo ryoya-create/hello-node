406f4e8f3488e68b2739a6844120c4a5b20649c4
dory <ryoyadoi.kyoto@gmail.com>
2022-07-03 00:57:34 +0900

    edit server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const  userRouter = require("./routes/user");
 
 const PORT = 3000;
 
+//app.use(mylogger);
+
 //静的ファイルを使う
 //app.use(express.static("public"));
 
@@ -22,5 +24,10 @@ app.use("/user", userRouter);
 //app.use("/auth", authRouter);
 //app.use("/customer", customerRouter);
 //app.use("/product", productRouter);
+
+//ミドルウェア
+function mylogger(req,res, next){
+    console.log(req.originalUrl);
+}
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
